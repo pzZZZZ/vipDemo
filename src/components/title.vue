aa5bb3d4f953679da5bebefb2230336f8c69585e
pz@hunch.ai <pz@hunch.ai>
2016-12-21 16:37:20 +0800

    Demo首次上传

---
+++ src/components/title.vue
@@ -0,0 +1,17 @@
+<template>
+
+  
+</template>
+
+<script>
+import Vue from 'vue';
+import { Cell } from 'mint-ui';
+Vue.component(Cell.name, Cell);
+
+  export default {
+    props: ['text', 'hide'],
+    mounted: function () {
+      document.title = this.text;
+    }
+  }
+</script>
